0662bfa4b02e24e887f8e2381b56984645f151a2
Faraz Khan <farazcsk@gmail.com>
2016-09-28 13:50:02 +0100

    Adds nav bar to app component

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TestCreator from './TestCreator';
 import WorkoutCreator from './WorkoutCreator';
+import AppBar from 'material-ui/AppBar';
 
 injectTapEventPlugin();
 
@@ -9,7 +11,9 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-			
+				<MuiThemeProvider>
+					<AppBar showMenuIconButton={false}/>
+				</MuiThemeProvider>
 			</div>
 		)
 	}
