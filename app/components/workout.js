4204509af66ace487e603d2a9fda2c8a8351345d
Faraz Khan <farazcsk@gmail.com>
2016-10-02 17:33:40 +0100

    add feature where when workout component is mounted the workout is fetched from api

--- app/components/workout.js
+++ app/components/workout.js
@@ -1,16 +1,32 @@
 import React, {Component} from "react";
+import $ from "jquery";
 
 class Workout extends Component {
 	constructor(props) {
 	  super(props);
 	
-	  this.state = {};
+	  this.state = {
+	  	workout: {}
+	  };
+	}
+
+	componentDidMount() {
+		$.ajax({
+      		url: "http://localhost:3000/api/Workouts/" + this.props.params.id + "?access_token=iTk6s6Boej92VgEFrKNnvg4rqD1uXjZmAUoNtHKgIqOwxi0LpnEToMK8SKYcjXuC",
+      		dataType: 'json',
+      		success: function(data) {
+        		this.setState({workout: data});
+      		}.bind(this),
+      		error: function(xhr, status, err) {
+        		console.error('#GET Error', status, err.toString());
+      		}.bind(this)
+    	});
 	}
 	render() {
 
 		return (
 			<div>
-				<h1>{this.props.params.id}</h1>
+				<h1>{this.state.workout.title}</h1>
 			</div>
 		)
 	}
