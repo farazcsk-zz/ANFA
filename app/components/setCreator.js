201c0fb6cc3c44ce456c5b2178c933bc486d4c81
Faraz Khan <farazcsk@gmail.com>
2016-10-05 16:03:54 +0100

    Adds dialog for creating sets/sections..

--- app/components/setCreator.js
+++ app/components/setCreator.js
@@ -9,6 +9,25 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
 class SetCreator extends Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	open: false
+	  };
+
+	  this.handleOpen = this.handleOpen.bind(this);
+	  this.handleClose = this.handleClose.bind(this);
+	}
+
+	handleOpen() {
+    	this.setState({open: true});
+ 	};
+
+  	handleClose() {
+    	this.setState({open: false});
+ 	};
+
 	render() {
 		const buttonStyles = {
 			backgroundColor: 'transparent',
@@ -18,23 +37,30 @@ class SetCreator extends Component {
 			}
 		}
 
-
 		const borderStyle = {
 			border: '2px solid #36BA93',
 			float: 'right'
 		}
+		
 		return (
 			<div>
-				<MuiThemeProvider>
-					<FlatButton 
-						label="Add Section"
-						style={borderStyle}
-						rippleColor={buttonStyles.rippleColor} 
-						backgroundColor={buttonStyles.backgroundColor} 
-						labelStyle={buttonStyles.labelStyle}
-						hoverColor={buttonStyles.backgroundColor}  
-					/>
-				</MuiThemeProvider>
+				<FlatButton 
+					label="Add Section"
+					style={borderStyle}
+					rippleColor={buttonStyles.rippleColor} 
+					backgroundColor={buttonStyles.backgroundColor} 
+					labelStyle={buttonStyles.labelStyle}
+					hoverColor={buttonStyles.backgroundColor}
+					onClick={this.handleOpen}  
+				/>
+				<Dialog
+		          title="Dialog With Actions"
+		          modal={false}
+		          open={this.state.open}
+		          onRequestClose={this.handleClose}
+		        >
+		          The actions in this window were passed in as an array of React objects.
+    			</Dialog>
 			</div>
 		)
 	}
