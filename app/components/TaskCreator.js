0b46692457cc61f48875bb9ee10fa17e0ae6749d
Faraz Khan <farazcsk@gmail.com>
2016-10-08 16:42:12 +0100

    WIP task creator

--- app/components/TaskCreator.js
+++ app/components/TaskCreator.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
 import $ from "jquery";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
+import FlatButton from 'material-ui/FlatButton';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import TextField from 'material-ui/TextField';
 import Divider from 'material-ui/Divider';
 import Grid from 'react-bootstrap/lib/Grid';
@@ -13,9 +15,11 @@ class TaskCreator extends Component {
 	  super(props);
 	
 	  this.state = {
-	  	question: '',
-	  	answer: '',
-	  	wrongAnswers: ['', '', '']
+	  	task: {
+	  		instructions: '',
+	  		answer: '',
+	  		wrongAnswers: ['', '', '']
+	  	}
 	  };
 
 	  this.onQuestionChange = this.onQuestionChange.bind(this);
@@ -27,20 +31,20 @@ class TaskCreator extends Component {
 	}
 
 	onQuestionChange(e) {
-		this.setState({question: e.target.value});
+		this.setState({task: { question: e.target.value}});
 	}
 
 	onAnswerChange(e) {
-		this.setState({answer: e.target.value});
+		this.setState({task: { answer: e.target.value}});
 	}
 
 	onWrongChange(e) {
 		if (e.target.id === 'wrongAnswer1') {
-			this.setState({wrongAnswers: [e.target.value, this.state.wrongAnswers[1], this.state.wrongAnswers[2]]});
+			this.setState({task:{wrongAnswers: [e.target.value, this.state.wrongAnswers[1], this.state.wrongAnswers[2]]}});
 		} else if (e.target.id === 'wrongAnswer2') {
-			this.setState({wrongAnswers: [this.state.wrongAnswers[0], e.target.value, this.state.wrongAnswers[2]]});
+			this.setState({task:{wrongAnswers: [this.state.wrongAnswers[0], e.target.value, this.state.wrongAnswers[2]]}});
 		} else {
-			this.setState({wrongAnswers: [this.state.wrongAnswers[0], this.state.wrongAnswers[1], e.target.value]});
+			this.setState({task:{wrongAnswers: [this.state.wrongAnswers[0], this.state.wrongAnswers[1], e.target.value]}});
 		}
 	}
 	
@@ -61,39 +65,159 @@ class TaskCreator extends Component {
 	}
 
 	render() {
+		const paperStyle = {
+		  height: '100%',
+		  width: 'inherit',
+		  margin: 20,
+		  padding: 10,
+		  display: 'inline-block',
+		  border: '2px solid #36BA93'
+		};
+
+		const buttonStyles = {
+			backgroundColor: 'transparent',
+			rippleColor: '#36BA93',
+			labelStyle: {
+				color: '#36333C',
+			}
+		}
+
+		const borderStyle = {
+			border: '2px solid #36BA93',
+			marginTop: '2.5vh',
+			marginLeft: '5vw',
+			width: 'inherit'
+		}
+
+		const inputStyles = {
+		  underlineStyle: {
+		    borderColor: '#36BA93',
+		  },
+		  floatingLabelFocusStyle: {
+		  	color: '#36BA93'
+		  }
+		};
+
+		const styles = {
+		  block: {
+		    maxWidth: 250,
+		  },
+		  radioButton: {
+		    marginBottom: 16,
+		  },
+		};
 		return (
 			<div>
 				<Grid>
 					<Row className="show-grid">
-						<Col md={8} mdOffset={2}>
-							<h1>Test Creator</h1>
+						<Col md={10}>
 							<MuiThemeProvider>
-								<form>
+								<Paper style={paperStyle}>
+									<TextField 
+		          						id="question" 
+		          						floatingLabelText="Enter task name" 
+		          						underlineFocusStyle={inputStyles.underlineStyle}
+										floatingLabelFocusStyle={inputStyles.floatingLabelFocusStyle}
+		          						onChange={this.onQuestionChange}
+	          						/>
+	          						<hr />
+	          						<h6>Select task type:</h6>
+	          						<RadioButtonGroup name="taskType" defaultSelected="Learn">
+		          						<RadioButton
+		          							value="Learn"
+	        								label="Learn"
+	  									/>
+	                                    <RadioButton
+	                                    	value="Questions"
+	        								label="Questions"
+	  									/>
+  									</RadioButtonGroup>
+								</Paper>
+							</MuiThemeProvider>
+							<MuiThemeProvider>
+								<Paper style={paperStyle}>
 									<Row className="show-grid">
 										<Col md={12}>
-				          					<TextField id="question" placeholder="Enter Question" onChange={this.onQuestionChange}/>
+				          					<TextField 
+				          						id="question" 
+				          						floatingLabelText="Instructions" 
+				          						multiLine={true}
+				          						underlineFocusStyle={inputStyles.underlineStyle}
+												floatingLabelFocusStyle={inputStyles.floatingLabelFocusStyle}
+				          						onChange={this.onQuestionChange}
+			          						/>
 				          				</Col>
 				          			</Row>
-				          			<br />
-				          			<br />
-				          			<br />
-				          			<Row className="show-grid">
-				          				<Col md={6}>
-				          					<TextField id="answer" placeholder="Enter Answer" onChange={this.onAnswerChange}/>
-										</Col>
+				        		</Paper>
+			        		</MuiThemeProvider>
 
-										<Col md={6}>
-					          				<TextField id="wrongAnswer1" placeholder="Enter Wrong Answer" onChange={this.onWrongChange}/>
-					          			
-				          					<TextField id="wrongAnswer2" placeholder="Enter Wrong Answer" onChange={this.onWrongChange}/>
+			        		<MuiThemeProvider>
+			        			<Paper style={paperStyle}>
+			        				<Row className="show-grid">
+				          				<Col md={12}>
+				          					<Row className="show-grid">
+				          						<Col md={6} mdOffset={3}>
+						          					<TextField 
+						          						id="answer" 
+						          						floatingLabelText="Enter Answer"
+						          						underlineFocusStyle={inputStyles.underlineStyle}
+														floatingLabelFocusStyle={inputStyles.floatingLabelFocusStyle} 
+						          						onChange={this.onAnswerChange}
+					          						/>
+				          						</Col>
+			          						</Row>
+			          						<Row className="show-grid">
+				          						<Col md={6} mdOffset={3}>
+							          				<TextField 
+							          					id="wrongAnswer1" 
+							          					floatingLabelText="Enter Wrong Answer"
+							          					underlineFocusStyle={inputStyles.underlineStyle}
+														floatingLabelFocusStyle={inputStyles.floatingLabelFocusStyle} 
+							          					onChange={this.onWrongChange}
+						          					/>
+					          					</Col>
+			          						</Row>
 
-					          				<TextField id="wrongAnswer3" placeholder="Enter Wrong Answer" onChange={this.onWrongChange}/>
+			          						<Row className="show-grid">
+				          						<Col md={6} mdOffset={3}>
+						          					<TextField 
+						          						id="wrongAnswer2" 
+						          						floatingLabelText="Enter Wrong Answer"
+						          						underlineFocusStyle={inputStyles.underlineStyle}
+														floatingLabelFocusStyle={inputStyles.floatingLabelFocusStyle} 
+						          						onChange={this.onWrongChange}
+					          						/>
+					          					</Col>
+			          						</Row>
+											
+											<Row className="show-grid">
+				          						<Col md={6} mdOffset={3}>
+							          				<TextField 
+							          					id="wrongAnswer3" 
+							          					floatingLabelText="Enter Wrong Answer"
+							          					underlineFocusStyle={inputStyles.underlineStyle}
+														floatingLabelFocusStyle={inputStyles.floatingLabelFocusStyle} 
+							          					onChange={this.onWrongChange}
+						          					/>
+					          					</Col>
+				          					</Row>
 				          				</Col>
 			          				</Row>
-				          			
-									<RaisedButton label="Create" onClick={this.handleSubmit} />
-				        		</form>
+			        			</Paper>
 			        		</MuiThemeProvider>
+	        			</Col>
+	        			<Col md={2}>
+	        				<MuiThemeProvider>
+	        					<FlatButton 
+									style={borderStyle}
+									rippleColor={buttonStyles.rippleColor} 
+									backgroundColor={buttonStyles.backgroundColor} 
+									labelStyle={buttonStyles.labelStyle}
+									hoverColor={buttonStyles.backgroundColor} 
+									label="Save" 
+									onClick={this.handleSubmit} 
+								/>
+							</MuiThemeProvider>
 	        			</Col>
 					</Row>
 	    		</Grid>
